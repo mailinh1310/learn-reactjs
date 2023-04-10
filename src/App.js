2bc4f25be6260cecaccf9d34b7d147bd9661c4f8
MLinh <mailinhhn1310@gmail.com>
2023-04-10 13:10:56 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mai Linh
         </p>
         <a
           className="App-link"
